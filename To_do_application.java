f1f3f9d95ce95401f8aed8d93a0535c6219ff6b0
Venky-51 <koppunavonivenkatesh51@gmail.com>
2025-06-07 19:30:02 +0530

    modified code

--- To_do_application.java
+++ To_do_application.java
@@ -1,92 +1,105 @@
-package Simple_To_Do_List;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class To_do_application{
-public static void main(String args[]){
-Scanner scan = new Scanner(System.in);
-ArrayList<String> list = new ArrayList<>();
+public class To_do_application {
 
-while(true){
-System.out.println("====TO DO LIST====");
-System.out.println("1.Add task");
-System.out.println("2.View Task");
-System.out.println("3.Delete Task");
-System.out.println("4.Exit");
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
 
-System.out.println();
-System.out.println("Enter your choice : ");
-int choice = scan.nextInt();
-scan.nextLine();
+        System.out.println("========== welcome to CONSOLE BASED TO DO APPLICATION ==========");
+        while(true){
+            System.out.println("1.Add task");
+            System.out.println("2.View task");
+            System.out.println("3.Delete task");
+            System.out.println("4.Exit");
+            System.out.println();
+            System.out.println("Enter your option : ");
 
-switch(choice){
-case 1:
-System.out.println("Enter the tasks to add and press -1 to stop : ");
-while(true){
-String task = scan.nextLine();
-if(task.equals("-1")){
-break;
+            int choice = scan.nextInt();
+            scan.nextLine();
+
+            switch(choice){
+                case 1:
+                System.out.println("Enter your tasks (type -1 to stop)");
+                while(true){
+                    String task = scan.nextLine();
+                    if(task.equals("-1")){
+                        break;
+                    }
+
+                    if(!task.trim().isEmpty()){
+                        list.add(task);
+                    }else{
+                        System.out.println("Invalid task to add");
+                    }
+                }
+                System.out.println("Tasks added successfully");
+                System.out.println();
+                break;
+
+
+                case 2:
+                if(list.isEmpty()){
+                    System.out.println("No tasks added yet");
+                }else{
+                    System.out.println("---------- Tasks list ---------");
+                    for(int i=0;i<list.size();i++){
+                        System.out.println((i+1)+"."+list.get(i));
+                    }
+                }
+                System.out.println();
+                break;
+
+
+                case 3:
+                if(list.isEmpty()){
+                    System.out.println("No tasks to delete");
+                }else{
+                    System.out.println("---------- Tasks list ----------");
+                    for(int i=0;i<list.size();i++){
+                        System.out.println((i+1)+"."+list.get(i));
+                    }
+
+                    System.out.println("Enter task number to remove(eg:-2 3 4 ) or -1 to cancel");
+                    String input = scan.nextLine().trim();
+                    if(input.equals("-1")){
+                        System.out.println("Remove cancelled");
+                        break;
+                    }
+
+                    String str_num_arr[] = input.split(" ");
+                    ArrayList<Integer> toRemove = new ArrayList<>();
+
+                    for(String str : str_num_arr){
+                     try{
+                        int n = Integer.parseInt(str);
+                        if(n >= 1  && n <= list.size()){
+                            toRemove.add(n-1);
+                        }else{
+                            System.out.println("Invalid task number : "+n);
+                        }
+                     }catch(NumberFormatException e){
+                        System.out.println("Invalid input: " + str);
+                     }
+                    }
+
+                    toRemove.sort((a,b)->b-a);
+                    for(int index : toRemove){
+                        System.out.println("Removed: " + list.remove(index));
+                    }
+                }
+                break;
+                
+
+                case 4:
+                System.out.println("Exiting CONSOLE BASED TO DO APPLICATION ,good bye buddy !!!!!");
+                scan.close();
+                System.exit(0);
+                break;
+
+            }
+        }
+    } 
 }
-
-if(!task.trim().isEmpty()){
-list.add(task);
-//System.out.println("Task added : "+task);
-}else{
-System.out.println("Empty task can't be added, try again");
-}
-}
-System.out.println("All tasks added to the list");
-System.out.println();
-break;
-
-
-case 2:
-if(list.isEmpty()){
-System.out.println("No tasks added yet...");
-}else{
-System.out.println("-----Your tasks In the List-----");
-for(int i=0;i<list.size();i++){
-System.out.println((i+1) + "." + list.get(i));
-}
-}
-System.out.println();
-break;
-
-
-case 3:
-if(list.isEmpty()){
-System.out.println("No tasks to Remove.");
-}else{
-System.out.println("-----Your tasks-----");
-for(int i=0;i<list.size();i++){
-System.out.println((i+1)+"."+list.get(i));
-}
-
-System.out.println("Enter the task number to remove");
-int taskNumber = scan.nextInt();
-scan.nextLine();
-
-if(taskNumber >=1 && taskNumber <= list.size()){
-String removedTask = list.remove(taskNumber - 1);
-System.out.println("Task : " + removedTask + " removed successfully! " );
-}else{
-System.out.println("Invalid Task number");
-}
-}
-System.out.println();
-break;
-
-
-case 4:
-System.out.println("Exiting To-Do list,  good bye buddy !!");
-scan.close();
-System.exit(0);
-break;
-}
-}
-
-
-
-}
-}
+     
